bf6294186e6f044704ff03eceaf9e73eac885488
keicy <kei.nakamura97@gmail.com>
2015-10-18 01:26:22 +0900

    Add function, read_request.

--- src/httpserver/httpd.c
+++ src/httpserver/httpd.c
@@ -1,3 +1,14 @@
+/****** Constants ********************************************************/
+
+#define SERVER_NAME "LittleHTTP"
+#define SERVER_VERSION "1.0"
+#define HTTP_MINOR_VERSION 0
+#define BLOCK_BUF_SIZE 1024
+#define LINE_BUF_SIZE 4096
+#define MAX_REQUEST_BODY_LENGTH (1024 * 1024)
+
+
+
 /****** Data Type Definitions ********************************************/
 
 /* it is copy of the HTTPRequest's structure. see P358,368. */
@@ -39,6 +50,36 @@ service(FILE *in, FILE *out, char *docroot){
   req = read_request(in);
   respond_to(req, out, docroot);
   free_request(req);
+}
+
+/* return struct's point. */
+static struct HTTPRequest*
+read_request(FILE *in){
+  struct HTTPRequest *req;
+  struct HTTPHeaderField *h;
+
+  req = xmalloc(sizeof(struct HTTPRequest));
+  // extract requestLine's data.
+  read_request_line(req, in);
+  // extract httpHeaders.
+  req->header = NULL;
+  while(h = read_header_field(in)){
+    h->next = req->header;
+    req->header = h;
+  }
+  // get content length.
+  req->length = content_length(req);
+  // extract content body.
+  if(req->length != 0){
+    if(req->length > MAX_REQUEST_BODY_LENGTH)
+      log_exit("request body too long");
+    req->body = xmalloc(req->length);
+    if(fread(req->body, req->length, 1, in) < 1) //cf.fread) http://bituse.info/c_func/43
+      log_exit("failed to read request body");
+  }els{
+    req->body = NULL;
+  }
+  return req;
 }
 
 /* *cahr and *struct should be freeed. */
